ef7c29ec4568c9cf9ef52a8643bc59234a07c721
liwenhao555 <719256863@qq.com>
2019-01-08 15:22:59 +0800

    【other】数据显示大致完成

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,50 +1,19 @@
 import React ,{PureComponent,Fragment} from "react";
 import {Progress} from "antd";
-import {Link} from "react-router-dom";
+import moment from "moment";
 import {requestApi } from "../../config";
 
 class Home extends PureComponent{
     state={
-        data:[{
-            number: '1',
-            needId:"Gb542",
-            needName:"对话弹框",
-            belongName:"百秀项目组",
-            startTime:"2018.12.12",
-            testTime:"2019.01.05",
-            endTime:"2019.1.8",
-            action:"" 
-          },
-          {
-            number: '2',
-            needId:"Gb542",
-            needName:"商品弹框",
-            belongName:"可得项目组",
-            startTime:"2018.12.26",
-            testTime:"2018.12.31",
-            endTime:"2019.1.9",
-            action:"" 
-          },
-          {
-            number: '3',
-            needId:"Gb542",
-            needName:"社区模块",
-            belongName:"Erp后台",
-            startTime:"2018.12.10",
-            testTime:"2018.12.16",
-            endTime:"2019.1.14",
-            action:"" 
-          }],
+        data:[],
           nowTime:""
     }
     componentDidMount(){
         let _this = this;
-        setInterval(()=>{
-            let time3 = Date.parse(new Date())/1000;//现在时间
-            _this.setState({nowTime:time3})
-        },1000)
         fetch(`${requestApi}/api/Home`).then(res=>res.json()).then(res=>{
-            console.log(res);
+            _this.setState({
+                data:res
+            })
         })
     }
     render(){
@@ -55,19 +24,25 @@ class Home extends PureComponent{
                         <div key={index} style={{marginBottom:"20px"}}>
                             <div style={{display:"flex",justifyContent:"flex-start",alignItems:"center"}}>
                                 <div style={{marginRight:"20px"}}>
-                                    需求编号<br/>{item.needId} 
+                                    需求编号<br/>{item.requertNo} 
                                 </div>
                                 <div style={{marginRight:"20px"}}>
-                                     需求名称<br/>{item.needName}
+                                     需求名称<br/>{item.requestName}
+                                </div>
+                                <div style={{marginRight:"20px"}}>
+                                     预发布时间<br/>{item.preReleaseTime}
                                 </div>
                                 <div style={{marginRight:"20px"}}>
                                     <Progress 
-                                        percent={this.onCountTime(item.startTime,item.testTime)} 
+                                        percent={this.onCountTime(item.prePercent)} 
                                         type="circle" 
                                         format={(percent) => percent>=100?"距离预发布已结束":"距离预发布"} ></Progress>
-                                    </div>
+                                </div>
+                                <div style={{marginRight:"20px"}}>
+                                     发布时间<br/>{item.releaseTime}
+                                </div>
                                     <Progress 
-                                        percent={this.onCountTime(item.startTime,item.endTime)} 
+                                        percent={this.onCountTime(item.relPercent)} 
                                         type="circle" 
                                         format={(percent) => percent>=100?"距离发布已结束":"距离发布"}
                                         ></Progress>
@@ -91,12 +66,12 @@ class Home extends PureComponent{
             </Fragment>
         )
     }
-    onCountTime=(start,end)=>{
-            let {nowTime} = this.state;
-            let time1 = Date.parse(new Date(end))/1000; //结束时间
-            let time2 = Date.parse(new Date(start))/1000;//开始时间
-            let xx=Number((nowTime-time2)/(time1-time2)*100).toFixed();
-            return xx;
+    onCountTime=(val)=>{
+        if(val>1){
+            return 100;
+        }
+        val = val*100
+        return val;
     }
 }
 
